90e014f1c5fb750a9521928849a710589f45ac74
Andrea Roizman <andrearoizman@MBP-de-Andrea.iptv.microsoft.com>
2018-04-03 12:20:30 -0300

    Edited output format

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,33 +3,44 @@ class ArticlesController < ApplicationController
 
   def index
     articles = Article.all.order("created_at DESC").limit(10)
-    render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
+    render json: articles, status: :ok
   end
 
   def show
     article = Article.find(params[:id])
-    render json: {status: 'SUCCESS', message: 'Loaded article', data:article}, status: :ok
+    render json: article, status: :ok
   end
 
   def create
     article = Article.new(article_params)
     if article.save
-      render json: {status: 'SUCCESS', message: 'Article saved', data:article}, status: :created
+      render json: article, status: :created
     else
-      render json: {status: 'ERROR', message: 'Article not saved', data:article.errors}, status: :unprocessable_entity
+      render json: article.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     article = Article.find(params[:id])
     article.destroy
-    render json: {status: 'SUCCESS', message: 'Article deleted', data:article}, status: :ok
+    render json: article, status: :ok
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.update_attributes(article_params)
+      render json: article, status: :ok
+
+    else
+      render json: article.errors, status: :unprocessable_entity
+    end
   end
 
   private
   def article_params
     params.permit(:title, :subtitle, :body)
   end
+
 
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,36 @@ class CommentsController < ApplicationController
 
   def index
     comments = Article.find(params[:article_id]).comments
-    render json: {status: 'SUCCESS', message: 'Loaded comments', data:comments}, status: :ok
+    render json: comments, status: :ok
   end
 
   def show
     comment = Article.find(params[:article_id]).comments.find(params[:id])
-    render json: {status: 'SUCCESS', message: 'Loaded comment', data:comment}, status: :ok
+    render json: comment, status: :ok
   end
 
   def create
     comment = Article.find(params[:article_id]).comments.new(comment_params)
     if comment.save
-      render json: {status: 'SUCCESS', message: 'Comment saved', data:comment}, status: :created
+      render json: comment, status: :created
     else
-      render json: {status: 'ERROR', message: 'Comment not saved', data:comment.errors}, status: :unprocessable_entity
+      render json: comment.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     comment = Article.find(params[:article_id]).comments.find(params[:id])
     comment.destroy
-    render json: {status: 'SUCCESS', message: 'Comment deleted', data:comment}, status: :ok
+    render json: comment, status: :ok
+  end
+
+  def update
+    comment = Article.find(params[:article_id]).comments.find(params[:id])
+    if comment.update_attributes(comment_params)
+      render json: comment, status: :ok
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
   end
 
   private
@@ -34,5 +43,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.permit(:comment, :author)
   end
+
 
 end
